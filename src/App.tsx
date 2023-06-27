9e5245bcefabef91d89a418407987bd3be8bf40f
Aldinei <aldinei.franco@unimedprudente.com.br>
2023-06-27 08:06:05 -0300

    up

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './paginas/Home';
+import VitrineRestaurantes from './paginas/VitrineRestaurantes';
+import { AdministracaoRestaurante } from './paginas/Administracao/Restaurante/AdministracaoRestaurante';
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+      <Route path="/admin/restaurantes" element={<AdministracaoRestaurante />} />
+    </Routes>
+  );
+}
+
+export default App;
